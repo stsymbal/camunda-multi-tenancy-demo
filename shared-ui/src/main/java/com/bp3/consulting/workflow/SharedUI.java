fdf98936cd3f9a08447ed6a6e13400ca2514b0a5
stsymbal <stsymbal@bp-3.com>
2021-09-28 11:56:45 -0500

    Cleanup

--- shared-ui/src/main/java/com/bp3/consulting/workflow/SharedUI.java
+++ shared-ui/src/main/java/com/bp3/consulting/workflow/SharedUI.java
@@ -1,15 +1,8 @@
 package com.bp3.consulting.workflow;
 
-import javax.sql.DataSource;
-
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.DependsOn;
-import org.springframework.context.annotation.Primary;
 
 
 @SpringBootApplication
