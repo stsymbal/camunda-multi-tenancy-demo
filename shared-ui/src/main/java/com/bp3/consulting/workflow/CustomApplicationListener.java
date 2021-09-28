bf4c362a9d087c82b9d1211c7cac52fed647e75a
stsymbal <stsymbal@bp-3.com>
2021-09-28 12:13:40 -0500

    Switch to @EventListener

--- shared-ui/src/main/java/com/bp3/consulting/workflow/CustomApplicationListener.java
+++ shared-ui/src/main/java/com/bp3/consulting/workflow/CustomApplicationListener.java
@@ -2,14 +2,13 @@ package com.bp3.consulting.workflow;
 
 import org.camunda.bpm.BpmPlatform;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CustomApplicationListener implements ApplicationListener<ApplicationReadyEvent> {
-
-    @Override
-    public void onApplicationEvent(final ApplicationReadyEvent event) {
+public class CustomApplicationListener {
+    @EventListener
+    public void onApplicationReadyEvent(final ApplicationReadyEvent event) {
         System.out.println("ProcessEngines=" + BpmPlatform.getProcessEngineService().getProcessEngineNames());
     }
 }
